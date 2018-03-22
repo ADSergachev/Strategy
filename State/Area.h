e190373661ca2d50ebd9a7ffcc98889fca16cc2c
ADSergachev <sergachevad@gmail.com>
2018-03-22 23:44:39 +0300

    add classes

---
+++ State/Area.h
@@ -0,0 +1,18 @@
+//
+// Created by Сергачев Андрей on 22.03.2018.
+//
+
+#ifndef LAB7_AREA_H
+#define LAB7_AREA_H
+
+
+#include "../People/Millitary.h"
+
+class Area : public Millitary{
+public:
+    explicit Area(double prosents) : Millitary(prosents) {}
+
+};
+
+
+#endif //LAB7_AREA_H
