f84c6c2c86fa26591e22cddfd46ec2910073c6cf
ADSergachev <sergachevad@gmail.com>
2018-03-22 23:43:20 +0300

    add classes

---
+++ People/Workers.h
@@ -0,0 +1,16 @@
+//
+// Created by Сергачев Андрей on 22.03.2018.
+//
+
+#ifndef LAB7_WORKERS_H
+#define LAB7_WORKERS_H
+
+
+#include "People.h"
+
+class Workers : public People{
+
+};
+
+
+#endif //LAB7_WORKERS_H
